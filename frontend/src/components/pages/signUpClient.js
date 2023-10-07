59b1a2a967de32e47505f4ca7654e6d80ea2a3c8
“Daryasse” <seliidarya@gmail.com>
2023-10-07 16:42:29 +0200

    signUp values added

--- frontend/src/components/pages/signUpClient.js
+++ frontend/src/components/pages/signUpClient.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect, handleSubmit  } from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -59,115 +60,150 @@ const defaultTheme = createTheme({
   }
 );
 
-export default function SignInSide() {
+function SignUpClient() {
+  const [firstName, setName] = useState('');
+  const [lastName, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputName= (event) => {
+    const stringValue = event.target.value;
+    setName(stringValue);
+  };
+  const handleInputSurname= (event) => {
+    const stringValue = event.target.value;
+    setSurname(stringValue);
+  };
+  const handleInputEmail= (event) => {
+    const stringValue = event.target.value;
+    setEmail(stringValue);
+  };
+  const handleInputPassword= (event) => {
+    const stringValue = event.target.value;
+    setPassword(stringValue);
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
       email: data.get('email'),
       password: data.get('password'),
     });
   };
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
 
-        <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography variant="title1">
-              SIGN IN
-            </Typography>
-            <Typography variant="subtitle1">To get started please enter your details.</Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-            <TextField
-                label="Name"
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                name="name"
-                autoComplete="name"
-                sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
-              />
+          <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Typography variant="title1">
+                SIGN IN
+              </Typography>
+              <Typography variant="subtitle1">To get started please enter your details.</Typography>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
-                label="Surname"
-                margin="normal"
-                required
-                fullWidth
-                id="surname"
-                name="surname"
-                autoComplete="surname"
-                sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
-              />
-              <TextField
-                label="Email"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                name="email"
-                autoComplete="email"
-                sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
-              />
-              <TextField
-                label="Password"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, backgroundColor: "#9CD91B",  }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+                  label="Name"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  name="name"
+                  autoComplete="name"
+                  value={firstName}
+                  onChange={handleInputName}
+                  sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
+                />
+                <TextField
+                  label="Surname"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="surname"
+                  name="surname"
+                  autoComplete="surname"
+                  value={lastName}
+                  onChange={handleInputSurname}
+                  sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
+                />
+                <TextField
+                  label="Email"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={handleInputEmail}
+                  sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
+                />
+                <TextField
+                  label="Password"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={handleInputPassword}
+                  sx = {{borderRadius: '8px', border: "1px solid #9CD91B", outline: '0'}}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, backgroundColor: "#9CD91B",  }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
+          <Grid item xs={false} sm={4} md={6}
+            sx={{
+              backgroundImage: `url(${image_login})`,
+              backgroundRepeat: 'no-repeat',
+              // backgroundColor: (t) =>
+              //   t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
         </Grid>
-        <Grid item xs={false} sm={4} md={6}
-          sx={{
-            backgroundImage: `url(${image_login})`,
-            backgroundRepeat: 'no-repeat',
-            // backgroundColor: (t) =>
-            //   t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-      </Grid>
-    </ThemeProvider>
-  );
+      </ThemeProvider>
+    );
 }
+
+export default SignUpClient;
+
+// export default SignUpClient;
